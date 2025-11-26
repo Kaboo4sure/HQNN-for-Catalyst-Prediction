f2e6d7dbf07b4332d3c7f4913e51aa7fa87b1409
kaboo4sure <kaboo4real@gmail.com>
2025-11-25 22:22:00 -0600

    pushing

--- src/Week6_final_pipeline.py
+++ src/Week6_final_pipeline.py
@@ -1,3 +1,6 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from src.week1_exploration import run_week1_exploration
 from src.week2_preprocessing import run_week2_preprocessing
 from src.week2b_consistency import run_week2b_consistency
--- src/week4_hqnn.py
+++ src/week4_hqnn.py
@@ -20,6 +20,9 @@ or via main pipeline:
   python .\main.py
 """
 
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import os
 import warnings
 
